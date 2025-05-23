f024f2557404ead301253e13d115840f3c405079
joaorocha <joao.santos@unitri.edu.br>
2025-05-23 16:51:40 -0300

    fix: home page imagens de serviços que oferecemos mudado a imagem dos cards

--- lib/home-page-variable.ts
+++ lib/home-page-variable.ts
@@ -1,32 +1,38 @@
+import cardKwai from '@/public/images/card-kwai.png'
+import cardTiktok from '@/public/images/card-tiktok.png'
+import cardYoutube from '@/public/images/card-youtube.png'
+import cardFacebook from '@/public/images/card-facebook.png'
+import cardInstagram from '@/public/images/card-instagram.png'
+
 export const socialServices = [
   {
     name: 'Instagram',
     description: 'Aumente seu alcance com seguidores reais e curtidas genuínas',
-    image: 'https://images.pexels.com/photos/4926674/pexels-photo-4926674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: cardInstagram.src,
     features: ['Seguidores Reais', 'Curtidas Orgânicas', 'Comentários Autênticos']
   },
   {
     name: 'Facebook',
     description: 'Expanda sua presença social com engajamento de qualidade',
-    image: 'https://images.pexels.com/photos/5797908/pexels-photo-5797908.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: cardFacebook.src,
     features: ['Seguidores Ativos', 'Reações Genuínas', 'Compartilhamentos']
   },
   {
     name: 'TikTok',
     description: 'Viralize seu conteúdo com visualizações e seguidores',
-    image: 'https://images.pexels.com/photos/6953867/pexels-photo-6953867.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: cardTiktok.src,
     features: ['Seguidores Reais', 'Visualizações', 'Likes']
   },
   {
     name: 'YouTube',
     description: 'Cresça seu canal com inscritos e visualizações reais',
-    image: 'https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: cardYoutube.src,
     features: ['Inscritos Ativos', 'Visualizações', 'Likes nos Vídeos']
   },
   {
     name: 'Kwai',
     description: 'Aumente sua audiência com seguidores engajados',
-    image: 'https://images.pexels.com/photos/3759097/pexels-photo-3759097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: cardKwai.src,
     features: ['Seguidores Reais', 'Curtidas', 'Comentários']
   }
 ]
