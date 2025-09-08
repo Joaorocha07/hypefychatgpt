47facf24ae3f089a8ec695912d7088351cfbed5e
João Rocha <joao.santos@unitri.edu.br>
2025-09-08 10:09:16 -0300

    fix: description atualizada

--- app/head.tsx
+++ app/head.tsx
@@ -9,7 +9,7 @@ export default function CustomHead({ title }: CustomHeadProps): JSX.Element {
     <>
       <title>{title}</title>
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <meta name="description" content="Sistema Integrado ASOEC" />
+      <meta name="description" content="Hype fy Agência - Serviços digitais" />
       <link
         rel="icon"
         href={logo.src}
