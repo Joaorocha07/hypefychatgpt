5a7bda8f4d848bb422fbacb887373125aeb9eafa
Joaorocha07 <rocha.joao.victor.50@gmail.com>
2025-10-19 23:22:11 -0300

    fix(head): Atualizado a forma de usar o head

--- app/head.tsx
+++ app/head.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head"
 import Script from "next/script"
 import { Analytics } from "@vercel/analytics/next"
 import logo from "@/public/images/hypefy.png"
@@ -10,12 +9,12 @@ interface CustomHeadProps {
 export default function CustomHead({ title }: CustomHeadProps): JSX.Element {
   return (
     <>
-      <Head>
+      <head>
         <title>{title}</title> 
         <meta content="width=device-width, initial-scale=1.0" name="viewport" /> 
         <meta name="description" content="Hype fy Agência - Serviços digitais" /> 
         <link rel="icon" href={logo.src} type="image/svg+xml" sizes="any" />
-      </Head>
+      </head>
 
       {/* Meta Pixel Code */}
       <Script id="meta-pixel" strategy="afterInteractive">
