b7b37289bf4a07eaa88c7559ae3a09b0f3ffac08
Joaorocha07 <rocha.joao.victor.50@gmail.com>
2025-10-19 23:16:27 -0300

    fix(head): Configurado para aparecer a imagem do usuario

--- app/head.tsx
+++ app/head.tsx
@@ -11,9 +11,9 @@ export default function CustomHead({ title }: CustomHeadProps): JSX.Element {
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <meta name="description" content="Hype fy Agência - Serviços digitais" />
+        <title>{title}</title> 
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" /> 
+        <meta name="description" content="Hype fy Agência - Serviços digitais" /> 
         <link rel="icon" href={logo.src} type="image/svg+xml" sizes="any" />
       </Head>
 
